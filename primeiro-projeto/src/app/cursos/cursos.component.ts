f6674dd25250363ef84f7896eb9b08cefb20c652
Felipe Lima <flf.felipe@gmail.com>
2017-09-03 19:40:46 -0300

    Introduction on 'Services'

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './cursos.service'
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,10 +10,14 @@ export class CursosComponent implements OnInit {
 
   nomePortal: string;  
 
-  cursos: string[] = ['Java', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) {
     this.nomePortal = 'http://localhost:4200';
+
+    // var servico = new CursosService();
+    // usa injeção de dependência
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
