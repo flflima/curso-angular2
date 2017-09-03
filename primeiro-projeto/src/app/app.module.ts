8b2959ee3bf50e8a80a97803b080ea06dfe929ff
Felipe Lima <flf.felipe@gmail.com>
2017-09-03 18:41:57 -0300

    Using modules

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from "./cursos/cursos.module";
 
 @NgModule({
-  declarations: [
+  declarations: [ 
+    // lista componentes, diretivas e pipes deste módulo
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    // outros módulos para este módulo, ou outro componente 
+    // deste módulo
+    BrowserModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], 
+  // serviços disponíveis à todos os componentes declarados
+  bootstrap: [AppComponent] 
+  // qual o componente que deve ser instanciado no boot do 
+  // projeto (apenas no módulo raiz)
 })
 export class AppModule { }
