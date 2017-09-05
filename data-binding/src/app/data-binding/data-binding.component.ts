35064398467f83529a2b570fc8cf506f9c090d31
Felipe Lima <felipe.freire@engdb.com.br>
2017-09-05 08:33:26 -0300

    Output Properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -24,6 +24,8 @@ export class DataBindingComponent implements OnInit {
 
   nomeDoCurso: string = "Angular";
 
+  valorInicial: number = 15;
+
   getValor() {
     return 1;
   }
@@ -42,6 +44,10 @@ export class DataBindingComponent implements OnInit {
 
   onMouseOverOut() {
     this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor (evento) {
+    console.log(evento.novoValor);
   }
 
   constructor() { }
