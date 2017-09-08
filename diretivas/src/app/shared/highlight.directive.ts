3bd8bfeafbd18e069784a6ed16c622268e6d31b3
Felipe Lima <flf.felipe@gmail.com>
2017-09-07 22:08:37 -0300

    Directives w/ Input and Property Binding

---
+++ diretivas/src/app/shared/highlight.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective implements OnInit {
+
+  @HostListener('mouseenter') onMouseOver() {
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  @Input() defaultColor: string = 'white';
+  @Input() highlightColor: string = 'yellow';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+  }
+}
